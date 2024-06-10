a6dc9ea07537c1781b26f3cd8374ed8d55a5b18b
Zeeshan Riaz <devzeeshanriaz@gmail.com>
2024-06-10 20:22:04 +0500

    ios fix

--- config/BasePath.js
+++ config/BasePath.js
@@ -1,10 +1,10 @@
 //Production
-const BasePath='https://clients.digitaldeposits.app/mobile/api/';
+const BasePath = 'https://clients.digitaldeposits.app/mobile/api/';
 // const BasePath = 'https://staging.digitaldeposits.app/';
 //Staging
-// const BasePath='https://staging.digitaldeposits.app/mobile/api/';
+// const BasePath = 'https://staging.digitaldeposits.app/mobile/api/';
 
-// const BasePath='http://localhost:5000/mobile/api/'
+// const BasePath='http://localhost:5000/mobile/api/' //local
 // https://clients.digitaldeposits.app
 // const BasePath = BasePathStaging;
 export default BasePath;
